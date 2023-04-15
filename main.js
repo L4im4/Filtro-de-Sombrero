5bc9025101dec610ad906dc8d238c2bbcab42938
L4im4 <113398829+L4im4@users.noreply.github.com>
2023-04-15 09:45:30 -0600

    Add files via upload
    Ultimisima parte

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+var ojox=0
+var ojoy=0
+
+function preload(){
+  sombrero=loadImage("Mexican hat.webp")
+
+}
+
 function setup(){
   canvas = createCanvas(400, 400);
   video = createCapture(VIDEO);
@@ -12,8 +20,11 @@ function model_loaded(){
 
 function posar(results){
 console.log(results)
+ojox=results[0].pose.leftEye.x-200
+ojoy=results[0].pose.leftEye.y-300
 }
 
 function draw(){
 image(video ,0,0,400,400)
+image(sombrero,ojox,ojoy,200,200)
 }
