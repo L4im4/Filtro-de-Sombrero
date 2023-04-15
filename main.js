558a17e776de06cc628cb5a51cadbdd7218d19f5
L4im4 <113398829+L4im4@users.noreply.github.com>
2023-04-15 09:17:39 -0600

    Cambios (NO es lo mismo)

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 function setup(){
-  createCanvas(400,400);
+  canvas = createCanvas(400, 400);
+  video = createCapture(VIDEO);
+    video.hide();
+    net=ml5.poseNet(video,model_loaded)
+    net.on("pose",posar )
 }
 
+function model_loaded(){
+ console.log("Model Loaded! ");
+}
+
+function posar(results){
+console.log(results)
+}
+
+function draw(){
+image(video ,0,0,400,400)
+}
